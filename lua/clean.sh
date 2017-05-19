d9725f62443b2d0c43f0d8adfad582e342c56a1d
Emil Dotchevski <emil@revergestudios.com>
2017-05-18 18:28:04 -0700

    lua

--- lua/clean.sh
+++ lua/clean.sh
@@ -1,4 +1,3 @@
 rm -r subprojects/packagecache
 rm -r subprojects/lua-5.1.5
 rm -r build
-unlink subprojects/lua.tar.gz
--- zlib/clean.sh
+++ zlib/clean.sh
@@ -1,4 +1,3 @@
 rm -r subprojects/packagecache
 rm -r subprojects/zlib-1.2.8
 rm -r build
-unlink subprojects/zlib.tar.gz
