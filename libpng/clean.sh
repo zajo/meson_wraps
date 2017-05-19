50ae251a2d43d9ffa8dca8044c0447a1ee085d4f
Emil Dotchevski <emil@revergestudios.com>
2017-05-18 17:47:51 -0700

    libpng

--- libpng/clean.sh
+++ libpng/clean.sh
@@ -1,3 +1,5 @@
 rm -r subprojects/packagecache
 rm -r subprojects/libpng-1.6.29
+rm -r subprojects/zlib-1.2.8
+unlink subprojects/libpng.tar.gz
 rm -r build
--- zlib/clean.sh
+++ zlib/clean.sh
@@ -1,3 +1,4 @@
 rm -r subprojects/packagecache
 rm -r subprojects/zlib-1.2.8
 rm -r build
+unlink subprojects/zlib.tar.gz
