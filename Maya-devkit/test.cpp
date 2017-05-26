a7f4a0928ff07d9edc54e8581f30dbd7dad8e4ff
Emil Dotchevski <emildotchevski@gmail.com>
2017-05-25 19:09:11 -0700

    Maya-devkit

--- Maya-devkit/test.cpp
+++ Maya-devkit/test.cpp
@@ -1,8 +1,18 @@
+#ifndef MAYA_VERSION
+#error Error in meson.build, it should #define MAYA_VERSION
+#endif
+
+#ifndef MAYA_LOCATION
+#error Error in meson.build, it should #define MAYA_LOCATION
+#endif
+
 #include "maya/MObject.h"
+#include <iostream>
 
 int
 main()
 	{
 	MObject mo;
+	std::cout << "MAYA_VERSION #defined as: \"" MAYA_VERSION "\"\nMAYA_LOCATION #defined as: \"" MAYA_LOCATION "\"\n";
 	return 0;
 	}
