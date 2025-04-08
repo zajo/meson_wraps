f14d32df62b3833116f165c3a70bc0c5e7a6e2f5
Emil Dotchevski <emildotchevski@gmail.com>
2025-04-07 23:44:17 -0400

    zlib 1.3.1

--- libpng/clean.sh
+++ libpng/clean.sh
@@ -1,4 +1,4 @@
 rm -r subprojects/packagecache
 rm -r subprojects/libpng-1.6.29
-rm -r subprojects/zlib-1.2.8
+rm -r subprojects/zlib-1.3.1
 rm -r build
--- zlib/clean.sh
+++ zlib/clean.sh
@@ -1,3 +1,3 @@
 rm -r subprojects/packagecache
-rm -r subprojects/zlib-1.2.8
+rm -r subprojects/zlib-1.3.1
 rm -r build
